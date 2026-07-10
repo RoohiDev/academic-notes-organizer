47100018db20ed8a244b0fd4a7223e67e4f605dc
Amirhossein Rouhi <amirhosseinroohicode@gmail.com>
2026-07-10 19:17:41 +0330

    Fix login redirect for authenticated users and clean up base template

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('accounts/login/', LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('notes.urls')),
 ]
