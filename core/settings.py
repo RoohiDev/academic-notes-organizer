1f10d818516b988b76943369879242bc93de7d15
Amirhossein Rouhi <amirhosseinroohicode@gmail.com>
2026-07-16 12:47:43 +0330

    Add leading slash to STATIC_URL to avoid broken static files

--- core/settings.py
+++ core/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 STATICFILES_DIR = [BASE_DIR / 'static']
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = 'notes:course_list'
